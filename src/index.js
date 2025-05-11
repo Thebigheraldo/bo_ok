41b2d5cc2eaf14f19f685e69c778ee0728f32a51
Herald <vogli86@icloud.com>
2025-05-11 22:40:16 +0200

    Aggiorno app: collegamento a Firestore e profili reali

--- src/index.js
+++ src/index.js
@@ -4,22 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { UserProvider } from './context/UserContext.js';
-
+import { UserProvider } from './context/UserContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>
 );
-root.render(
-  <UserProvider>
-    <App />
-  </UserProvider>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
